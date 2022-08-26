2eab18b67fba527df952457719c3991bc30b56e1
Wesley Oliveira Silva <wesolsv@gmail.com>
2022-08-25 22:51:50 -0300

    Inclusao para a logica da escolha do ganhador, perdedor e empate.

--- app/src/main/java/com/wesolsv/jokenpogame/MainActivity.java
+++ app/src/main/java/com/wesolsv/jokenpogame/MainActivity.java
@@ -2,10 +2,10 @@ package com.wesolsv.jokenpogame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,16 +21,19 @@ public class MainActivity extends AppCompatActivity {
                 this.opcaoSelecionada("pedra");
                 break;
             case 2131231211:
-                this.opcaoSelecionada("papel");;
+                this.opcaoSelecionada("papel");
                 break;
             case 2131231213:
                 this.opcaoSelecionada("tesoura");
                 break;
+            default:
+                System.out.println("Opção Inválida");
         }
     }
 
-    public void opcaoSelecionada(String opcaoSelecionada){
+    public void opcaoSelecionada(String opcaoUsuario){
         ImageView imageResultado = findViewById(R.id.resultadoImg);
+        TextView resultado = findViewById(R.id.resultadoTxt);
 
         String[] opcoesApp = {"pedra","papel","tesoura"};
         String escolhaApp = opcoesApp[(int) (Math.random() * 3)];
@@ -47,6 +50,19 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
+        if(     (escolhaApp.equals("tesoura")  && opcaoUsuario.equals("papel") )||
+                (escolhaApp.equals("papel") && opcaoUsuario.equals("pedra") )||
+                (escolhaApp.equals("pedra") && opcaoUsuario.equals("tesoura"))
+        ){
+            resultado.setText(getString(R.string.you_lose));
+
+        }else if((opcaoUsuario.equals("tesoura") && escolhaApp.equals("papel")) ||
+                (opcaoUsuario.equals("papel") && escolhaApp.equals("pedra")) ||
+                (opcaoUsuario.equals("pedra") && escolhaApp.equals("tesoura") )){
+            resultado.setText(getString(R.string.you_win));
+        }else {
+            resultado.setText(getString(R.string.a_tie));
+        }
 
     }
 }
